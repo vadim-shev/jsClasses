9c7c8f12ad1b881d20a7af1956d75ada59682488
Vadim <58843818+vadim-shev@users.noreply.github.com>
2024-09-21 15:19:03 +0300

    g

--- createButton.js
+++ createButton.js
@@ -1,31 +1,48 @@
 
-
-// const buttonManager = new ButtonManager(set ID, set innerHTML)
-
-
-class ButtonManager {
-    constructor(buttonId, buttonText, parentElement = document.body) {
-        this.buttonId = buttonId
-        this.buttonText = buttonText
+class ElementManager {
+    constructor(tagName, elementId, parentElement = document.body) {
+        this.tagName = tagName
+        this.elementId = elementId
         this.parentElement = parentElement
     }
 
-    createButton() {
-        const btn = document.createElement("button")
+    create() {
+        const element = document.createElement(this.tagName)
 
-        btn.id = this.buttonId
-        btn.innerHTML = this.buttonText
+        element.id = this.elementId
 
-        this.parentElement.appendChild(btn)
+        this.parentElement.appendChild(element)
+    }
+    addCSSClass(classNames) { 
+        const element = document.getElementById(this.elementId)
+
+        if (element) {
+            classNames.split(' ').forEach(className => {
+                element.classList.add(className)
+            })
+        } else {
+            console.error(`Element with ID ${this.elementId} not found.`)
+        }
+    }
+    insertElementBefore(tag) {
+        const element = document.getElementById(this.elementId)
+        const newElement = document.createElement(tag) 
+
+        if (this.parentElement && element) {
+            this.parentElement.insertBefore(newElement, element) 
+        } else {
+            console.error(`Element with ID ${this.elementId} not found or parent element is not set.`)
+        }
     }
 
-    addClickListener(callback) {
-        const button = document.getElementById(this.buttonId)
+    putInElement(tag) {
+        const element = document.getElementById(this.elementId)
+        const newElement = document.createElement(tag) 
 
-        if (button) {
-            button.addEventListener('click', callback)
-        } else {
-            console.error(`Button with ID ${this.buttonId} not found.`)
-        }
+        element.appendChild(newElement)
+    }
+    writeInText(text) {
+        const element = document.getElementById(this.elementId)
+        element.innerHTML = text
     }
 }
